99e281ec009e61eea1d02573a2c3f895fad8441e
Micah J <justmic007>
2020-07-12 22:07:54 +0100

    code restructure, clean up and removed dead codes

--- src/server.ts
+++ src/server.ts
@@ -1,69 +1,31 @@
 import express from 'express';
 import logger from 'morgan';
-import bodyParser from 'body-parser';
-
-// import knex from './knex'
-// import routes from './src/hostels/routes';
-
-// import { ApolloServer, gql } from 'apollo-server-express';
-// import graphql from 'graphql';
+import helmet from 'helmet';
 import cors from 'cors';
+// import { ApolloServer, gql } from 'apollo-server-express';
+
+import routes from './endpoints/routes';
 
 const app = express();
-// knex.queryBuilder();
+
+app.use(helmet());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false })); // Check if to be true or false
-
-
-// import 'dotenv/config';
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-// const schema = gql`
-//   type Book {
-//     title: String
-//     author: String
-//   }
-
-//   type Query {
-//     books: [Book]
-//   }
-// `;
-
-// const books = [
-//   {
-//     title: 'Harry Potter and the Chamber of Secrets',
-//     author: 'J.K. Rowling',
-//   },
-//   {
-//     title: 'Jurassic Park',
-//     author: 'Michael Crichton',
-//   },
-// ];
-
-// const resolvers = {
-//   Query: {
-//     books: () => books,
-//   },
-// };
-
-// const server = new ApolloServer({ typeDefs: schema, resolvers });
-
-// server.applyMiddleware({ app });
-
 app.use(cors());
-const port = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
 
 
+app.use('/upathlearning', routes);
+
+app.use('/upathlearning', (req, res) => {
+  res.send('Welcome to UPATH')
+})
+
+app.all('/*', (req, res) => {
+  res.send('Path cannot be reached');
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
-  console.log(`\Server listening on port ---> ${port}\n`)
+  console.log(`\n Server listening on port ---> ${port}\n`)
 );
-
-
-// const port = process.env.PORT || 3000;
-// server.listen(port, () => {
-//   console.log(`\n=== Server listening on port ${port}\n`);
-// });
